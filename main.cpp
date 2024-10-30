73f05615908f38222db2f2c9b84c8c0bac0c8f19
den251965 <74070947+den251965@users.noreply.github.com>
2024-10-30 14:52:02 +0300

    ver 1

--- main.cpp
+++ main.cpp
@@ -4,69 +4,93 @@
 
 #include "testcllass.h"
 
-
 int main() {
-  TestObj<int> test;
-  int it = 100;
-  test.set(it);
-  TestObj<int> test_5(it);
-  std::cout << "test = " << test.get() << " test_5 = " << test_5.get() << "\n";
+  {
+    TestObj<int> test;
+    int it = 100;
+    test.set(&it);
+    TestObj<int> test_5(it);
+    std::cout << "test = " << *test.get() << " test_5 = " << *test_5.get() << "\n";
 
-  std::cout << "it Upload" << it << " ";
+    std::cout << "it Upload " << it << " ";
+    it = 56;
+    std::cout << " -> " << it << "\n";
+    std::cout << "test = " << *test.get() << " test_5 = " << *test_5.get() << "\n";
 
-  test = 56;
-  std::cout << " -> " << it << "\n";
-  std::cout << "test = " << test.get() << " test_5 = " << test_5.get() << "\n";
+    std::cout << "---------------------------------  " << "\n";
 
-  std::cout << "---------------------------------  " << "\n";
+    TestObj<int> test_2(test);
+    std::cout << "test_2.item_ = " << *test_2.get() << " test.item_  = " << *test.get() << "\n";
 
-  TestObj<int> test_2(std::move(test));
-  std::cout << "test_2.item_ = " << test_2.get() << " test.item_  = " << test.get() << "\n";
+    std::cout << "move-------------------------  " << "\n";
 
-  std::cout << "---------------------------------  " << "\n";
+    TestObj<int> test_3(std::move(test_5));
+    std::cout << "test_3.item_ = " << *test_3.get() << " test_5.item_ - valid ? " << bool(test_5)
+              << "\n";
+    std::cout << "test_3 -> test_5 " << "\n";
+    test_5 = std::move(test_3);
+    std::cout << " test_5.item_ - valid ? " << bool(test_5) << " " << *test_5.get() << "\n";
 
-  TestObj<int> test_3(std::move(test_5));
-  std::cout << "test_3.item_ = " << test_3.get() << " test_5.item_ = " << test_5.get() << "\n";
-  std::cout << "test_3 -> test_5 " << "\n";
-  test_5 = std::move(test_3);
-  std::cout << test_5.get() << " " << test_5.get() << "\n";
+    std::cout << "---------------------------------  " << "\n";
+    {
+      std::cout << " std::vector<TestObj<int>> vec \n";
+      std::vector<TestObj<int>> vec;
+      vec.push_back(test_2);             // 56
+      vec.push_back(std::move(test_5));  // 100
+      vec.push_back(test);               // 56
+      vec.push_back(2);                  // 2
+      std::cout << " print vec \n";
+      for (auto& m : vec) {
+        std::cout << *m.get() << " ";
+      }
+      std::cout << "\n";
+    }
+    std::cout << "---------------------------------  " << "\n";
+    {
+      std::cout << " std::set<TestObj<int>> sec \n";
+      test_3 = test_2;
+      std::set<TestObj<int>> sec;
+      sec.insert(test_2);
+      sec.insert(std::move(test_3));
+      sec.insert(test);
+      sec.insert(5);
+      std::cout << " print set \n";
+      for (auto& m : sec) {
+        std::cout << *m.get() << " ";
+      }
+      std::cout << "\n";
+    }
+    std::cout << "---------------------------------  " << "\n";
 
-  std::cout << "---------------------------------  " << "\n";
+    TestObj<int> test_4;
+    test_4 = test_2;
+    std::cout << "test_4.item_ = " << *test_4.get() << "\n";
 
-  std::cout << " std::vector<TestObj<int>> vec \n";
-  std::vector<TestObj<int>> vec;
-  vec.push_back(test_2);
-  vec.push_back(std::move(test_5));
-  vec.push_back(test);
-  vec.push_back(2);
-  std::cout << " print vec \n";
-  for (auto& m : vec) {
-    std::cout << m.get() << " ";
+    std::cout << "---------------------------------  " << "\n";
   }
-  std::cout << "\n";
+  {
+    std::cout << "Test of structured ----------------------  " << "\n";
+    struct klin {
+      int olo = 0;
+      std::string ghj = "";
+    };
 
-  std::cout << "---------------------------------  " << "\n";
+    klin ts({.olo = 12, .ghj = "read"});
 
-  std::cout << " std::set<TestObj<int>> vec \n";
-  std::set<TestObj<int>> sec;
-  sec.insert(test_2);
-  sec.insert(std::move(test_5));
-  sec.insert(test);
-  sec.insert(2);
-  std::cout << " print set \n";
-  for (auto& m : vec) {
-    std::cout << m.get() << " ";
+    TestObj<klin> struct_test(ts);
+    std::vector<TestObj<klin>> vec;
+ std::cout << "std::vector<TestObj<klin>> vec ----------------------  " << "\n";
+    vec.push_back(klin{.olo = 15, .ghj = "second"});
+    vec.push_back(klin{.olo = 17, .ghj = "zara"});
+    vec.push_back(klin{.olo = 14, .ghj = "first"});
+    vec.push_back(ts);
+
+    std::cout << " print vec \n";
+    for (auto& m : vec) {
+      std::cout << m.get()->olo << " " << m.get()->ghj << "\n";
+    }
+    std::cout << "\n";
   }
-  std::cout << "\n";
-
-  std::cout << "---------------------------------  " << "\n";
-
-  TestObj<int> test_4;
-  test_4 = test_2;
-  std::cout << "test_4.item_ = " << test_4.get() << "\n";
-
-  std::cout << "---------------------------------  " << "\n";
-
   std::cout << "Succeses!!!\n";
   return 0;
 }
--- testcllass.h
+++ testcllass.h
@@ -5,84 +5,103 @@
 template <typename T>
 class TestObj {
 public:
-  TestObj() = default;
+  TestObj() {
+    item_ = new T;
+  }
 
   TestObj(const T item) {
-    item_ = item;
-    std::cout << "create const T " << item_ << "\n";
+    item_ = new T(item);
+    std::cout << "create const T " << "\n";
   }
 
   TestObj(T* item) {
     if (item)
       item_ = *item;
-    std::cout << "create *item " << item_ << "\n";
+    std::cout << "create *item " << "\n";
   }
 
   TestObj(const TestObj& other) {
-    item_ = other.item_;
-    std::cout << "create &TestObj " << item_ << "\n";
+    std::cout << "create &TestObj " << "\n";
+    delete item_;
+    T temp = *other.item_;
+    item_ = new T(temp);
   }
 
   TestObj(TestObj&& other) {
-    std::cout << "create &&TestObj" << other.item_ << "\n";
-    std::swap(item_, other.item_);
+    std::cout << "create &&TestObj" << "\n";
+    item_ = other.item_;
+    other.item_ = nullptr;
   }
 
-  TestObj& operator=(const TestObj& rhs) {
+  ~TestObj() {
+    std::cout << "deleted" << "\n";
+    delete item_;
+    item_ = nullptr;
+  }
+
+  TestObj& operator=(const TestObj& rhs) noexcept {
     std::cout << "operator =& \n";
     item_ = rhs.item_;
     return *this;
   }
 
-  TestObj& operator=(TestObj&& other) {
+  TestObj& operator=(TestObj&& other) noexcept {
     std::cout << "operator =moved \n";
-    if (this == &other) {
-      return *this;
+    if (this != &other) {
+      delete item_;
+      item_ = std::exchange(other.item_, nullptr);
     }
-    item_ = other.item_;
-    other.item_;
     return *this;
   }
 
-  bool operator<(const TestObj& rhs) const {
+  bool operator<(const TestObj& rhs) const noexcept {
     std::cout << "operator < \n";
-    return item_ < rhs.item_;
+    return *item_ < *rhs.item_;
   }
 
-  bool operator<=(const TestObj& rhs) const {
+  bool operator<=(const TestObj& rhs) const noexcept {
     std::cout << "operator <= \n";
-    return item_ <= rhs.item_;
+    return *item_ <= *rhs.item_;
   }
 
-  bool operator==(const TestObj& rhs) const {
+  bool operator==(const TestObj& rhs) const noexcept {
     std::cout << "operator == \n";
-    return item_ == rhs.item_;
-  }
-  
-  bool operator!=(const TestObj& rhs) const {
-    std::cout << "operator == \n";
-    return !(item_ == rhs);
+    return *item_ == *rhs.item_;
   }
 
-  bool operator>(const TestObj& rhs) const {
+  bool operator!=(const TestObj& rhs) const noexcept {
+    std::cout << "operator == \n";
+    return !(this == rhs);
+  }
+
+  bool operator>(const TestObj& rhs) const noexcept {
     std::cout << "operator < \n";
-    return !(item_ <= rhs);
+    return !(this <= rhs);
   }
 
-  bool operator>=(const TestObj& rhs) const {
+  bool operator>=(const TestObj& rhs) const noexcept {
     std::cout << "operator <= \n";
-    return !(item_ < rhs);
+    return !(this < rhs);
   }
 
-
-  void set(const T item) {
+  void set(T* item) {
     item_ = item;
   }
 
-  T get() const {
+  T* get() const {
     return item_;
   }
 
+  explicit operator bool() const {
+    return (item_) ? true : false;
+  }
+
+  void swap(TestObj& other) noexcept {
+    T* temp = other.item_;
+    other.item_ = item_;
+    item_ = temp;
+  }
+
 private:
-  T item_ = NULL;
+  T* item_ = nullptr;
 };
